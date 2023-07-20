5bb48d8b78f0c79e2b67c6a5f06f61716272d8cf
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2023-07-20 10:05:26 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, render_template
+import pandas
+import validators
+import os
+import json
+from werkzeug.utils import secure_filename
+import shortuuid
+import threading
+import time
+from pytube import YouTube
+import concurrent.futures
+
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'csv'}
+UPLOAD_FOLDER = "static/uploads"
+
+
+def cleanGarbage():
+    threading.Timer(600, cleanGarbage).start()
+    for file in os.listdir(UPLOAD_FOLDER):
+        if file != ".gitkeep":
+            os.remove(os.path.join(UPLOAD_FOLDER, file))
+    print(f'Garbage Cleaned at {time.strftime("%m/%d/%Y, %H:%M:%S")}.')
+
+
+cleanGarbage()
+
+
+def getYtVideoDownloadUrl(ytUrl):
+    try:
+        yt = YouTube(ytUrl)
+        streams = yt.streams
+        stream = streams.filter(progressive=True, type="video", file_extension="mp4")
+        stream = stream.order_by("resolution").desc().first()
+        return stream.url
+    except:
+        print(f"Can't download {ytUrl}")
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/api", methods=["GET", "POST"])
+def api():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return {"error": "No File Part."}
+
+        file = request.files['file']
+
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return {"error": "No File Selected."}
+
+        if not allowed_file(file.filename):
+            return {"error": "Please enter a CSV File."}
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filename = ".".join(filename.split(".")[:-1]) + "_" + shortuuid.uuid() + "." + filename.split(".")[-1]
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+            # reading the CSV file
+            csvFile = pandas.read_csv(os.path.join(UPLOAD_FOLDER, filename))
+            csvFileList = csvFile.values.tolist()
+
+            # displaying the contents of the CSV file
+            urlList = list()
+            for data in csvFileList:
+                if validators.url(str(data[0])):
+                    urlList.append(data[0])
+
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                results = executor.map(getYtVideoDownloadUrl, urlList)
+                urlList = list(results)
+
+            return json.dumps({"success": urlList})
+    else:
+        return {"error": "Please make a valid Request."}
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# No match found for pattern: var for={(.*?)}; is from pytube module, not the fault of this app.
+# This app uses pytube from the GitHub repository https://github.com/pulkitpareek18/pytube
+
+app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+let alertContainer = document.getElementById('alert')
+let spinner = document.getElementById("spinner")
+let downloadBtn = document.getElementById("downloadBtn")
+
+function showToast(backgroundClass, message) {
+  alertContainer.innerHTML = `<div class="toast show align-items-center text-bg-${backgroundClass} border-0" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+                ${message}
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            </div>`
+}
+
+function downloadVideos(videoUrls) {
+  for (var i = 0; i < videoUrls.length; i++) {
+    var videoUrl = videoUrls[i];
+    var link = document.createElement('a');
+    link.href = videoUrl;
+    link.target = '_blank';
+    link.click();
+  }
+}
+
+let responseDataUrls = []
+
+$("form#data").submit(function (e) {
+  e.preventDefault();
+  Pace.restart()
+  spinner.style.display = "inline-block"
+  downloadBtn.style.display = "none"
+  var formData = new FormData(this);
+  alertContainer.innerHTML = ""
+
+  $.ajax({
+    url: "/api",
+    type: 'POST',
+    data: formData,
+    success: function (data) {
+      let res = JSON.parse(data)
+      if (res.success) {
+        responseDataUrls = res.success
+        showToast("success","Click the Download Button Below to Download Videos.")
+      }
+      spinner.style.display = "none"
+      downloadBtn.style.display = "block"
+    },
+    cache: false,
+    contentType: false,
+    processData: false
+  });
+});
+
+$(document).ajaxError(function () {
+  alertContainer.innerHTML = `<div class="toast show align-items-center text-bg-danger border-0" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="d-flex">
+          <div class="toast-body">
+          No Internet Connection.
+          </div>
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      </div>`});
+
+
+downloadBtn.onclick = function(){
+  downloadVideos(responseDataUrls)
+};
